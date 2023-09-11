59f72778f3e1836fde62f11b1ad7948241489e9d
VeganC0de <danibobadilla@gmail.com>
2023-09-11 09:00:44 -0300

    Agregando Conversiones

---
+++ Conversiones/src/conversiones/Conversiones.java
@@ -0,0 +1,42 @@
+
+package conversiones;
+
+public class Conversiones {
+
+    
+    public static void main(String[] args) {
+        
+        //double num = 1.67;
+        
+        //casteo a entero
+        
+        //int  numInt = (int) num;
+        
+        //casteo a long
+        
+        //long numLong = (long) num;
+        
+        //System.out.println("Double: " + num);
+        //System.out.println("Int: " + numInt);
+        //System.out.println("Long: " + numLong);
+        
+        
+        //String cantidad = "15";
+        //String precio = "150.27";
+        
+        //int cantEntero = Integer.parseInt(cantidad);
+        //double precioDouble = Double.parseDouble(precio);
+        
+           //System.out.println("El valor total de la compra es :" + (cantEntero * precioDouble));
+           
+           
+        int edad = 40;
+        double estatura = 1.67;
+        
+        String edadString = String.valueOf(edad);
+        String estaturaString = String.valueOf(1.67);
+        
+        System.out.println("Edad: " + edadString +" Estatura: " + estaturaString);
+    }
+    
+}
